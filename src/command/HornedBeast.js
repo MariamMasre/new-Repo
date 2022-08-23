77e47eb01a3e2a053be9f898d069b75cbadc50d1
MariamMasre <meremmasre@gmail.com>
2022-08-23 05:02:37 +0300

    add lab class-02

--- src/command/HornedBeast.js
+++ src/command/HornedBeast.js
@@ -1,19 +1,57 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
+class HornedBeast extends React.Component {
 
-class HornedBeast extends React.Component{
-    render(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      numOfPets: 0
+
+    }
+  }
+  incrementNumOfPets = () => {
+    this.setState({
+      numOfPets: this.state.numOfPets + 1
+    })
+
+  }
+  render() {
+
+    return (
+      // <div>
+      //<h2>{this.props.title}</h2>
+      //<p>{this.props.disc}</p>
+      //<img src={this.props.img} alt={this.props.title}></img>
+      //<p>Num of pets: 0</p>
+      //<button>go some where</button>
+      //</div>
       
-    return(
-     <div>
-      <h2>{this.props.title}</h2>
-      <p>{this.props.disc}</p>
-      <img src={this.props.img} alt={this.props.title}></img>
-      </div>
-    )
+            <Card style={{ width: '18rem' }}>
+             
+                <Card.Img variant="top" src={this.props.img} onClick={this.incrementNumOfPets} />
+                <Card.Body>
+                  <Card.Title>{this.props.title}</Card.Title>
+                  <Card.Text>
+                    ❤️ Num of pets: {this.state.numOfPets}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+             
+            </Card>
+         
+        )
+     
+
+
     
-    }
-    
-    }
-    
-    export default HornedBeast;
+
+  }
+
+}
+
+export default HornedBeast;
+
+
--- src/command/Main.js
+++ src/command/Main.js
@@ -1,22 +1,67 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import uniWhal from "./img/Unicorn_and_Narwhal_by_dinglehopper.jpg";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+class Main extends React.Component {
+  render() {
 
-class Main extends React.Component{
-    render(){
-      
-    return(
-     <div>
-        
-        <HornedBeast title='UniWhal' img='http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg' disc='A unicorn and a narwhal nuzzling their horns'></HornedBeast>
-       
-       
-       <HornedBeast title='Rhino Family' img='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80' disc='Parent rhino with two babies'></HornedBeast>
+    return (
+      <div>
 
+        <CardGroup>
+          <HornedBeast title='UniWhal' img={uniWhal} disc='A unicorn and a narwhal nuzzling their horns' horns='1'></HornedBeast>
+
+          <HornedBeast title='Rhino Family' img='https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80' disc='Parent rhino with two babies'></HornedBeast>
+
+          <HornedBeast title='Unicorn Head' img='https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg' disc='Someone wearing a creepy unicorn head mask' horns='1'></HornedBeast>
+
+          <HornedBeast title='UniLego' img='https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4836a6fca62e7dce9324346bacfde085&auto=format&fit=crop&w=2534&q=80' disc='Lego figurine dressed in a unicorn outfit' horns='1'></HornedBeast>
+
+
+          <HornedBeast title='Basically a unicorn' img='https://i.pinimg.com/736x/b4/61/06/b46106830b841017ea59870b27ec18dc--narwhals-a-unicorn.jpg' disc='A narwhal is basically a unicorn after all, right?' horns='1'></HornedBeast>
+        </CardGroup>
+        <CardGroup>
+          <HornedBeast title='#truth' img='https://i.pinimg.com/originals/16/cf/2a/16cf2a0b3fd51b9bee08bb6296193b75.jpg' disc='The truth behind narwhals' horns='1'></HornedBeast>
+
+          <HornedBeast title='Baby Rhino' img='https://secure.img1-ag.wfcdn.com/im/17007094/resize-h800%5Ecompr-r85/3589/35892451/Baby+Rhino+Figurine.jpg' disc='This is actually a figurine but it looks kinda real' horns='2'></HornedBeast>
+
+          <HornedBeast title='Cera' img='https://vignette.wikia.nocookie.net/landbeforetime/images/c/c3/Cera_infobox.png/revision/latest?cb=20180422005003' disc='Three horns but still, horns. And who doesn like The Land Before Time?' horns='3'></HornedBeast>
+
+
+          <HornedBeast title='Narwhal costume' img='https://ae01.alicdn.com/kf/HTB18GwSQVXXXXaZaXXXq6xXFXXXh/Animal-Cosplay-Costume-Narwhal-Onesie-Mens-Womens-Cartoon-Whale-Pajamas.jpg' disc='A woman wearing a blue narwhal costume' horns='1'></HornedBeast>
+
+          <HornedBeast title='Rhino costume' img='https://www.shopmascot.com/image/cache/mascotnew/new196-800x800.jpg' disc='Mascots have to get their costumes somewhere' horns='2'></HornedBeast>
+        </CardGroup>
+
+        <CardGroup>
+          <HornedBeast title='Believe' img='https://www.tinselbox.com/wp-content/uploads/2018/03/I-BELIEVE-IN-UNICORNS-FREE-PRINTABLE-WATERCOLOR-7-of-1.jpg' disc='I believe in unicorns, do you?' horns='1'></HornedBeast>
+
+          <HornedBeast title='Markhor' img='https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg/220px-Markhor_Schraubenziege_Capra_falconeri_Zoo_Augsburg-02.jpg' disc='These wild goats eat snakes, then secrete a foam that locals fight over for the antivemon properties -- how cool is that?' horns='2'></HornedBeast>
+
+          <HornedBeast title='Baby markhor' img='http://www.zooborns.com/.a/6a010535647bf3970b0223c84d5959200c-800wi' disc='Even the babies are adorable' horns='2'></HornedBeast>
+
+          <HornedBeast title='Mouflon' img='https://images.unsplash.com/photo-1558560063-931ca9822a0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' disc='Those horns though' horns='2'></HornedBeast>
+
+          <HornedBeast title='Addax' img='https://images.unsplash.com/photo-1556890077-020ec300d5db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80' disc='This guy is basically extinct but survives well in captivity, so they frequently found in zoos' horns='2'></HornedBeast>
+        </CardGroup>
+        <CardGroup>
+          <HornedBeast title='Baby mouflon' img='https://cbsnews3.cbsistatic.com/hub/i/r/2013/03/05/5b414225-a645-11e2-a3f0-029118418759/thumbnail/620x350/2d4cf24685b45c22912e64d2004fec8d/Baby_Mouflon_Wild_Sheep.jpg' disc='The cuteness that is a baby mouflon asleep' horns='2'></HornedBeast>
+
+          <HornedBeast title='Happy Jackson Chameleon' img='https://images.unsplash.com/photo-1514036783265-fba9577fc473?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' disc='These are really common in Hawaii' horns='2'></HornedBeast>
+
+          <HornedBeast title='Serious Jackson  Chameleon' img='https://imgc.allpostersimages.com/img/print/posters/dlillc-jackson-s-chameleon_a-G-13448768-14258384.jpg' disc='This one is very serious.' horns='3'></HornedBeast>
+
+          <HornedBeast title='Horned Lizard' img='https://www.nps.gov/band/learn/nature/images/short-horned-lizard-open-mouth-18.jpg?maxwidth=650&autorotate=false' disc='Fave food: ants' horns='100'></HornedBeast>
+
+          <HornedBeast title='Smaug' img='https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/290px-Smaug_par_David_Demaret.jpg' disc='Fan illustration of Smaug from The Hobbit' horns='100'></HornedBeast>
+        </CardGroup>
       </div>
     )
-    
-    }
-    
-    }
-    
-    export default Main;
+
+  }
+
+}
+
+export default Main;
+
